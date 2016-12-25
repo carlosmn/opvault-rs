8243320599b1dfc3784f777ba9ae17195bf2578f
Carlos Martín Nieto <cmn@dwim.me>
2016-12-25 10:56:52 +0000

    Don't decrypt folders until asked

--- src/folder.rs
+++ src/folder.rs
@@ -10,7 +10,7 @@ use super::Result;
 use super::DerivedKey;
 
 #[derive(Debug, RustcDecodable)]
-pub struct Folder {
+pub struct FolderData {
     pub created: i64,
     pub overview: String,
     pub tx: i64,
@@ -19,18 +19,51 @@ pub struct Folder {
     pub smart: Option<bool>,
 }
 
-pub fn read_folders(p: &Path, key: &DerivedKey) -> Result<HashMap<String,Folder>> {
+#[derive(Debug)]
+pub struct Folder {
+    pub created: i64,
+    pub overview: Vec<u8>,
+    pub tx: i64,
+    pub updated: i64,
+    pub uuid: String,
+    pub smart: Option<bool>,
+}
+
+impl Folder {
+    fn from_folder_data(d: FolderData) -> Result<Folder> {
+        let overview = try!(d.overview.from_base64());
+        Ok(Folder {
+            created: d.created,
+            overview: overview,
+            tx: d.tx,
+            updated: d.updated,
+            uuid: d.uuid,
+            smart: d.smart,
+        })
+    }
+
+    /// Decrypt the folder's overview data given the overview keys
+    pub fn decrypt_overview(&self, key: &DerivedKey) -> Result<Vec<u8>> {
+        match opdata01::decrypt(&self.overview[..], &key.encrypt, &key.hmac) {
+            Ok(x) => Ok(x),
+            Err(e) => Err(From::from(e)),
+        }
+    }
+}
+
+/// Read the encrypted folder data
+pub fn read_folders(p: &Path) -> Result<HashMap<String, Folder>> {
     let mut f = try!(File::open(p));
     let mut s = String::new();
     try!(f.read_to_string(&mut s));
     // the file looks like it's meant to be eval'ed by a JS engine, which sounds
     // like a particularly bad idea, let's remove the non-json bits.
     let json_str = s.trim_left_matches("loadFolders(").trim_right_matches(");");
-    let mut folders: HashMap<String,Folder> = try!(json::decode(json_str));
+    let mut folder_datas: HashMap<String, FolderData> = try!(json::decode(json_str));
+    let mut folders = HashMap::new();
 
-    for (_, folder) in folders.iter_mut() {
-        let overview_data = try!(folder.overview.from_base64());
-        folder.overview = try!(String::from_utf8(try!(opdata01::decrypt(overview_data.as_slice(), &key.encrypt, &key.hmac))));
+    for (k, v) in folder_datas.drain() {
+        folders.insert(k, try!(Folder::from_folder_data(v)));
     }
 
     Ok(folders)
